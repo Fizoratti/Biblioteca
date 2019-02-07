bf241d213b77d81b3ef9604c486d0bb25d31c962
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 15:49:22 -0200

    Create Devolucao class

---
+++ src/Devolucao.java
@@ -0,0 +1,35 @@
+public class Devolucao {
+    private Vizinho vizinho;
+    private Livro livro;
+    private String data;
+    
+    public Devolucao(Vizinho vizinho, Livro livro) {
+        setVizinho(vizinho);
+        setLivro(livro);
+        this.data = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date());
+    }
+
+    public Vizinho getVizinho() {
+        return this.vizinho;
+    }
+    public void setVizinho(Vizinho vizinho) {
+        this.vizinho = vizinho;
+    }
+    public Livro getLivro() {
+        return this.livro;
+    }
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+    public String getData() {
+        return this.data;
+    }
+
+    @Override
+    public String toString() {
+        String devolucao = "(Vizinho): " + this.vizinho.getNome() + 
+                        "\n (Livro): " + this.livro.getTitulo() +
+                        "\n (Data): " + this.data;
+        return devolucao;
+    }
+}
