808b449758e303b3d0a3308a0fc8e155ed9b6f35
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 05:23:01 -0200

    Make the system load the database on start

--- src/App.java
+++ src/App.java
@@ -2,6 +2,8 @@ public class App {
     public static void main(String args[]) {
         System.out.print("Bem-vindo à Biblioteca Ágil!\n");
 
+        Database.start();
+
         Menu.getInstance().showMenu();
 
     }
