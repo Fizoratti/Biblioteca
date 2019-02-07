09f5f2e5d3fa3a23467e83816bf6bfadcf6ae3d2
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 16:02:10 -0200

    Create the Doacao class

---
+++ src/Doacao.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Doacao {
+    private Vizinho vizinho;
+    private Livro livro;
+    private String data;
+    
+    public Doacao(Vizinho vizinho, Livro livro, String data) {
+        setVizinho(vizinho);
+        setLivro(livro);
+        setData(data);
+    }
+    public Doacao(Vizinho vizinho, Livro livro) {
+        setVizinho(vizinho);
+        setLivro(livro);
+        this.data = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date());
+    }
+
+    public Vizinho getVizinho() {
+        return this.vizinho;
+    }
+    public void setVizinho(Vizinho vizinho) {
+        this.vizinho = vizinho;
+    }
+    public Livro getLivro() {
+        return this.livro;
+    }
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+    public String getData() {
+        return this.data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        String doacao = "(Vizinho): " + this.vizinho.getNome() + 
+                        "\n (Livro): " + this.livro.getTitulo() +
+                        "\n (Data): " + this.data;
+        return doacao;
+    }
+}
