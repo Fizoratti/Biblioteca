de900675602933f5a10e9ecf725023d585fb018b
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 15:22:10 -0200

    Add Getters, Setters and toString methods to Emprestimo class

--- src/Emprestimo.java
+++ src/Emprestimo.java
@@ -5,11 +5,35 @@ import java.text.SimpleDateFormat;
 public class Emprestimo {
     private Vizinho vizinho;
     private Livro livro;
-    private Date data;
+    private String data;
     
     public Emprestimo(Vizinho vizinho, Livro livro) {
         setVizinho(vizinho);
         setLivro(livro);
         this.data = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(new Date());
+    }
+
+    public Vizinho getVizinho() {
+        return this.vizinho;
+    }
+    public void setVizinho(Vizinho vizinho) {
+        this.vizinho = vizinho;
+    }
+    public Livro getLivro() {
+        return this.livro;
+    }
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+    public String getData() {
+        return this.data;
+    }
+
+    @Override
+    public String toString() {
+        String emprestimo = "(Vizinho): " + this.vizinho.getNome() + 
+                        "\n (Livro): " + this.livro.getTitulo() +
+                        "\n (Data): " + this.data;
+        return emprestimo;
     }
 }
