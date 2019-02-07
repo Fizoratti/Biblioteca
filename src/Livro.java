a309829b0014dcb6bf7439a0976cbafdec7aad5b
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 05:20:52 -0200

    Fix a typo in the constructor

--- src/Livro.java
+++ src/Livro.java
@@ -5,10 +5,11 @@ public class Livro {
     private int anoPublicacao;
 
     // Construtor
-    public livro(int cod, String tit, Autor aut, int ano) {
+    public Livro(int cod, String tit, Autor aut, int ano) {
         codigo = cod;
         titulo = tit;
         autor = aut;
         anoPublicacao = ano;
     }
+
 }
