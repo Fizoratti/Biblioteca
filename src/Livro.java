7d5106b0bc59b6679520ebd2667b8eedc5d3ef1c
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 06:44:44 -0200

    Add get/set methods for Livro

--- src/Livro.java
+++ src/Livro.java
@@ -2,12 +2,38 @@ public class Livro {
     private int codigo;
     private String titulo;
     private int anoPublicacao;
+    private Autor autor;
 
     // Construtor
     public Livro(int cod, String tit, int ano) {
         codigo = cod;
         titulo = tit;
         anoPublicacao = ano;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public String getTitulo() {
+        return this.titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAnoPublicacao() {
+        return this.anoPublicacao;
+    }
+    public void setAnoPublicacao(int ano) {
+        this.anoPublicacao = ano;
+    }
+    public Autor getAutor(){
+        return this.autor;
+    }
+    public void setAutor(Autor autor) {
+        this.autor = autor;
     }
 
 }
