98974f0d38458c3388e9eb45da1d44de4b0e169d
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 05:21:32 -0200

    Add implementation for the Vizinho class

---
+++ src/Vizinho.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Vizinho extends Pessoa {
+    private ArrayList<Emprestimo> emprestimos;
+
+    public Vizinho(String nome) {
+        super(nome);
+    }
+}
