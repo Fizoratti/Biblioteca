17b12361e79e3d753ae906d67d9733faeab91d9b
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 06:16:35 -0200

    Add codigo atribute to vizinho

--- src/Vizinho.java
+++ src/Vizinho.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
 
 public class Vizinho extends Pessoa {
+    private int codigo;
     private ArrayList<Emprestimo> emprestimos;
 
-    public Vizinho(String nome) {
+    public Vizinho(int cod, String nome) {
         super(nome);
+        setCodigo(cod);
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
     }
 }
