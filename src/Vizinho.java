cd4aef82bae118364b73658505506ed5dd49fd57
Felipe Bittencourt <robson.felipe@acad.pucrs.br>
2019-02-07 15:26:20 -0200

    Remove emprestimos list atribute from vizinho and implement a method for that

--- src/Vizinho.java
+++ src/Vizinho.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Vizinho extends Pessoa {
     private int codigo;
-    private ArrayList<Emprestimo> emprestimos;
 
     public Vizinho(int cod, String nome) {
         super(nome);
@@ -14,5 +15,20 @@ public class Vizinho extends Pessoa {
     }
     public void setCodigo(int codigo) {
         this.codigo = codigo;
+    }
+
+    public List<Emprestimo> getEmprestimos() {
+        List<Emprestimo> emprestimos = Database.getInstance().getEmprestimos()
+                                        .stream()
+                                        .filter(e -> e.getVizinho().getCodigo() == this.codigo)
+                                        .collect(Collectors.toList());
+        return emprestimos;
+    }
+
+    @Override
+    public String toString() {
+        String vizinho = "(Código): " + this.codigo + 
+                        "\n (Nome): " + super.getNome();
+        return vizinho;
     }
 }
